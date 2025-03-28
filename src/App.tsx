19e7264af9a0ea7dd79d00c5739accf40a7d5d3b
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2025-03-28 14:46:39 -0300

    Variáveis e Reset de CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('OI');
   return (
     <>
-      <Heading />
+      <Heading attr={1 + 1} attr2='String'>
+        Olá Mundo 1
+      </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere ex ipsa
         perferendis sed, dolorum qui officiis sint enim minus ducimus
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,9 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
+export function Heading(props) {
   return (
-    <h1 className={`${styles.heading} ${styles.cyan}`}>
-      Olá Mundo (do Heading)!
+    <h1 className={styles.heading}>
+      {props.children} | Soma: {props.attr} | Attr2: {props.attr2}
     </h1>
   );
 }
