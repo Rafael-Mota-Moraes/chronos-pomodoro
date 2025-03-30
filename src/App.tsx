a51c8f70bb5085bc4e322c3ecd709f4b72de21a2
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2025-03-30 12:36:44 -0300

    Input: um componente um pouco mais complexo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -21,8 +22,7 @@ export function App() {
       <Container>
         <form action='' className='form'>
           <div className='formRow'>
-            <label htmlFor='input'>Task</label>
-            <input type='text' id='input' />
+            <DefaultInput type='text' id='input' />
           </div>
           <div className='formRow'>
             <p>Lorem ipsum dolor sit amet</p>
