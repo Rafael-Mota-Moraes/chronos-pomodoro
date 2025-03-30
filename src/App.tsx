b3d116e4c4a93489ea8ba5b516e128b2356a87f3
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2025-03-30 11:24:42 -0300

    Criando o formulário da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,24 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <label htmlFor='input'>Task</label>
+            <input type='text' id='input' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
