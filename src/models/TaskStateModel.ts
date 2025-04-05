05b8542c521cdfd41bccffa866858f121df1bbf6
Rafael-Mota-Moraes-Moreira <rafaelmota622@gmail.com>
2025-04-05 13:25:39 -0300

    Definindo os models (modelos) da aplicação TaskModel e TaskStateModel

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,14 @@
+import { TaskModel } from './TaskModel';
+
+export type TaskStateModel = {
+  tasks: TaskModel[];
+  secondsRemaining: number;
+  formatedSecondsRemaining: string;
+  activeTask: TaskModel | null;
+  currentCircle: number;
+  config: {
+    workTime: number;
+    shortBreakTime: number;
+    longBreakTime: number;
+  };
+};
