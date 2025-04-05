fca145f3546122e8da81f32c81ba43913326eadd
Rafael-Mota-Moraes-Moreira <rafaelmota622@gmail.com>
2025-04-05 17:33:22 -0300

    Estado global da aplicação com useState e comunicação via props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+import { useState } from 'react';
+import { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formatedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <NotFound />;
+  const [state, setState] = useState<TaskStateModel>(initialState);
+
+  return <Home setState={setState} state={state} />;
 }
