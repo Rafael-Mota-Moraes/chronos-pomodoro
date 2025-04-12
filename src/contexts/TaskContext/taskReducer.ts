e08d25aca2410ba58cd60219476dc3c06869dfc1
Rafael-Mota-Moraes-Moreira <rafaelmota622@gmail.com>
2025-04-12 10:54:49 -0300

    Migrando o state de useState para useReducer

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -1,13 +1,36 @@
 import { TaskStateModel } from '../../models/TaskStateModel';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
+import { getNextCycle } from '../../utils/getNextCycle';
 import { TaskActionModel, TaskActionsTypes } from './taskActions';
 
 export function taskReducer(state: TaskStateModel, action: TaskActionModel) {
   switch (action.type) {
     case TaskActionsTypes.START_TASK: {
-      return state;
+      const nextCycle = getNextCycle(state.currentCycle);
+      const newTask = action.payload;
+      const secondsRemaining = newTask.duration * 60;
+      return {
+        ...state,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining: secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...state.tasks, newTask],
+      };
     }
     case TaskActionsTypes.INTERRUPT_TASK: {
-      return state;
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: '00:00',
+        tasks: state.tasks.map(task => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
+      };
     }
     case TaskActionsTypes.RESET_STATE: {
       action.type;
