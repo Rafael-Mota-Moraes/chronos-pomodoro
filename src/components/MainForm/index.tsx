b63b503a456ac2773036a41326ded2b6e21504c8
Rafael-Mota-Moraes-Moreira <rafaelmota622@gmail.com>
2025-04-12 11:16:55 -0300

    Criando o componente Tips para dar sugestões ao usuário sobre a tarefa e ciclo

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionsTypes } from '../../contexts/TaskContext/taskActions';
+import { Tips } from '../Tips';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -15,6 +16,8 @@ export function MainForm() {
   // ciclos
   const nextCycle = getNextCycle(state.currentCycle);
   const nextCycleType = getNextCycleType(nextCycle);
+
+  // Tips
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -62,7 +65,7 @@ export function MainForm() {
         />
       </div>
       <div className='formRow'>
-        <p>Próximo intervalo é de 25min</p>
+        <Tips />
       </div>
       {state.currentCycle > 0 && (
         <div className='formRow'>
