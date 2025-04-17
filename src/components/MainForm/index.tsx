eaadd7c76abb4093c65aaabc3027fd7f12181aac
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2025-04-17 13:53:14 -0300

    Atualizando o título da página a cada segundo da tarefa ativa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -14,6 +14,8 @@ import { showMessage } from '../../adapters/showMessage';
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+  const lastTaskName = state.tasks[(state.tasks.length = 1)]?.name || '';
+
   // ciclos
   const nextCycle = getNextCycle(state.currentCycle);
   const nextCycleType = getNextCycleType(nextCycle);
@@ -67,6 +69,7 @@ export function MainForm() {
           placeholder='Digite algo'
           ref={taskNameInput}
           disabled={!!state.activeTask}
+          defaultValue={lastTaskName}
         />
       </div>
       <div className='formRow'>
